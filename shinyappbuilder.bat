f35be4eddb2413f2dfb770f438c4ce38148e8eb9
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-02-14 12:46:03 +0100

    Updated bat file

--- shinyappbuilder.bat
+++ shinyappbuilder.bat
@@ -1,7 +1,9 @@
 @echo off
 
 D:/SoftwareDevelopment/VirtualEnvironments/ShinyAppBuilder/Scripts/python.exe D:/SoftwareDevelopment/GitHub/ShinyAppBuilder/shinyappbuilder.py ^
-    MyShinyApp ^
+    HPBdashboard ^
     D:/SoftwareDevelopment/GitHub/Dashboards/apps/hpbdashboard ^
     D:/SoftwareDevelopment/R/R-4.4.2-portable ^
     D:/SoftwareDevelopment
+
+pause
